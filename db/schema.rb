70910da7da4ccebc628f2aa5eb600a9c172d3f09
James Brennan <james@carbonfive.com>
2015-09-28 11:00:28 -0700

    Add schema

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+ActiveRecord::Schema.define(:version => 20120521144648) do
+
+  create_table "exercises", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "metrics"
+  end
+
+  create_table "routine_sets", :force => true do |t|
+    t.integer  "routine_id"
+    t.integer  "exercise_id"
+    t.string   "instructions"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "routines", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "order"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "workout_sets", :force => true do |t|
+    t.integer  "workout_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.integer  "routine_set_id"
+    t.string   "metrics"
+  end
+
+  create_table "workouts", :force => true do |t|
+    t.integer  "routine_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.boolean  "completed"
+    t.integer  "user_id"
+  end
+
+end
